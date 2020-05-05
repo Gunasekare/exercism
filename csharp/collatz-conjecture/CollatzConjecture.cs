d74e04cb77c214c77e6baba6fab1c0e37a29d333
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-05 21:39:49 +0530

    Collatz Conjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    static int steps = 0;
+    
+    public static int Steps(int number)
+    {   
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException();
+        
+        else if (number == 1)
+        {   
+            int value = steps;
+            steps = 0;
+            return value;
+        }
+        else if (number % 2 == 0)
+        {
+            ++steps;
+            return Steps(number / 2);
+        }
+        else
+        {
+            ++steps;
+            return Steps(3 * number + 1);
+        }
+    }
+}
