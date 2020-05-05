5acd322fb2692556141db5bedbd7e959154cb20f
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-05 19:19:18 +0530

    Perfect Numbers

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        List<int> factors = Factor(number);
+        int aliquotSum = 0;
+
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException();
+        
+        foreach (int value in factors)
+            aliquotSum += value;
+        
+        if (aliquotSum == number)
+            return Classification.Perfect;
+        else if (aliquotSum > number)
+            return Classification.Abundant;
+        
+        return Classification.Deficient;
+    }
+
+    public static List<int> Factor (int number)
+    {
+        List<int> factors = new List<int>() {0};
+
+        for (int i = 1; i <= (number / 2); ++i)
+            if (number % i == 0)
+                factors.Add(i);
+
+        return factors;
+    }
+}
