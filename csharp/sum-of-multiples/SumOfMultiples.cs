f508ecc29b4dddae68af7130b51723cb9b2bf7db
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-17 22:19:10 +0530

    Sum of Multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        List<int> factorsList = new List<int>();
+        int sum = 0;
+
+        foreach (int value in multiples)
+        {
+            if (value != 0)
+            {
+                int factor = value;
+                for (int i = 2; factor < max; factor = value * i++)
+                    if (!factorsList.Contains(factor))
+                        factorsList.Add(factor);
+            }
+        }
+
+        foreach (int value in factorsList)
+            sum += value;
+
+        return sum;
+    }
+}
