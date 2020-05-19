249efea141e1f7039bb785fed578e443f934df44
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-19 19:46:41 +0530

    Acronym

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string acronym = "";
+        string[] splitString = phrase.Replace("-", " ").Replace("_","").Split(" ");
+
+        foreach (string value in splitString)
+            if (value.Length > 0)
+                acronym += value[0];
+
+        return acronym.ToUpper();
+    }
+}
